f4d0196fa2b025969e9b224cf4bfa1b6c3282df3
NKwatra <kwatranishkarsh@gmail.com>
2020-10-31 18:50:48 +0530

    create attendance detail calendar

--- screens/AttendanceDetailScreen.js
+++ screens/AttendanceDetailScreen.js
@@ -1,5 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Image, TouchableOpacity, Text} from 'react-native';
+import {Calendar} from 'react-native-calendars';
+
+const CalendarArrow = (props) => {
+  return props.direction === 'left' ? (
+    <Image source={require('../assets/images/left-angle-black.png')} />
+  ) : (
+    <Image source={require('../assets/images/right-angle-black.png')} />
+  );
+};
+
+const calendarTheme = {
+  backgroundColor: '#F3E2CA',
+  calendarBackground: '#F3E2CA',
+  textSectionTitleColor: '#000000',
+  todayTextColor: '#00adf5',
+  textDisabledColor: '#7A7585',
+  dayTextColor: '#000000',
+  monthTextColor: '#000000',
+  textDayFontFamily: 'IBMPlexSans-Medium',
+  textMonthFontFamily: 'IBMPlexSans-Regular',
+  textDayHeaderFontFamily: 'IBMPlexSans-Bold',
+  textDayFontWeight: '500',
+  textMonthFontWeight: '400',
+  textDayHeaderFontWeight: '700',
+  textDayFontSize: 14,
+  textMonthFontSize: 16,
+  textDayHeaderFontSize: 10,
+};
 
 const AttendanceDetailScreen = ({navigation, route}) => {
   const tabOptions =
@@ -9,7 +37,49 @@ const AttendanceDetailScreen = ({navigation, route}) => {
 
   const [state, updateState] = useState({
     selectedIndex: 0,
+    attendance: {
+      2020: {
+        11: {
+          present: [15, 17, 21, 20, 12],
+          absent: [10, 13],
+        },
+        12: {
+          present: [1, 20, 31, 12, 15],
+          absent: [4, 9],
+        },
+      },
+    },
+    markedDates: {},
   });
+
+  const updateMarkedDates = (month, year) => {
+    if (month < 10) {
+      month = '0' + month;
+    }
+    let tempDates = {};
+    if (
+      !state.attendance.hasOwnProperty(year) ||
+      !state.attendance[year].hasOwnProperty(month)
+    ) {
+      return;
+    }
+
+    const {present, absent} = state.attendance[year][month];
+    for (let date of present) {
+      if (date < 10) {
+        date = '0' + date;
+      }
+      const dateString = year + '-' + month + '-' + date;
+      tempDates[dateString] = {selected: true, selectedColor: '#87C289'};
+    }
+
+    for (let date of absent) {
+      const dateString = year + '-' + month + '-' + date;
+      tempDates[dateString] = {selected: true, selectedColor: '#ED7479'};
+    }
+
+    updateState({...state, markedDates: tempDates});
+  };
 
   return (
     <View style={styles.container}>
@@ -41,7 +111,8 @@ const AttendanceDetailScreen = ({navigation, route}) => {
                 index === 0 ? styles.firstTab : {},
                 index === tabOptions.length - 1 ? styles.lastTab : {},
               ]}
-              onPress={() => updateState({...state, selectedIndex: index})}>
+              onPress={() => updateState({...state, selectedIndex: index})}
+              key={index}>
               <Text
                 style={[
                   styles.tabHeader,
@@ -53,6 +124,16 @@ const AttendanceDetailScreen = ({navigation, route}) => {
           );
         })}
       </View>
+      {state.selectedIndex === 0 ? (
+        <Calendar
+          onMonthChange={({month, year}) => updateMarkedDates(month, year)}
+          renderArrow={(direction) => <CalendarArrow direction={direction} />}
+          enableSwipeMonths={true}
+          theme={calendarTheme}
+          style={styles.calendarContainer}
+          markedDates={state.markedDates}
+        />
+      ) : null}
     </View>
   );
 };
@@ -116,6 +197,13 @@ const styles = StyleSheet.create({
   },
   selectedText: {
     color: '#000000',
+  },
+  calendarContainer: {
+    padding: 32,
+    backgroundColor: '#F3E2CA',
+    borderRadius: 20,
+    marginTop: 24,
+    marginHorizontal: -15,
   },
 });
 
